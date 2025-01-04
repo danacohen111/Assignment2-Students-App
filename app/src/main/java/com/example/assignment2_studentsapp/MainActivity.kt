0fc3fc210c9b6d0fd52611bf504ee72e679ddd38
danacohen111 <danac2121@gmail.com>
2025-01-04 21:51:26 +0200

    Add student adapter and create the recyclerview

--- app/src/main/java/com/example/assignment2_studentsapp/MainActivity.kt
+++ app/src/main/java/com/example/assignment2_studentsapp/MainActivity.kt
@@ -5,23 +5,39 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.assignment2_studentsapp.adapters.StudentAdapter
 import com.example.assignment2_studentsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var studentAdapter: StudentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupRecyclerView()
+
         binding.addStudentButton.setOnClickListener {
             startActivity(Intent(this, AddStudentActivity::class.java))
         }
     }
 
+    private fun setupRecyclerView() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        studentAdapter = StudentAdapter(StudentRepository.students) { student ->
+            val intent = Intent(this, StudentDetailsActivity::class.java)
+            intent.putExtra("STUDENT_ID", student.id)
+            startActivity(intent)
+        }
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = studentAdapter
+    }
     override fun onResume() {
         super.onResume()
         Log.d("StudentRepository", "Students: ${StudentRepository.students}")
+        studentAdapter.notifyDataSetChanged()
     }
 }
